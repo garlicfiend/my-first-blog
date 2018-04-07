9a4a8870b129c4757faf366fed92f3ea9e31d923
garlicfiend <garlicfiend@gmail.com>
2018-04-07 10:15:14 -0700

    Changed the html fot the site.

--- gsite/settings.py
+++ gsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*87v)f9_4nh=rm)+u5ekjv)b-6737e)(=-mapv#qhmr#-7jxc!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'garlicfiend.pythonanywhere.com']
 
 
 # Application definition
